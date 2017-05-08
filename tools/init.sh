05a70d02910bc4b45e61f8081001b00358b4819d
Adrian Toncean <adrianton3@gmail.com>
2017-05-08 23:39:35 +0200

    Make init script create the scratch project

--- tools/init.sh
+++ tools/init.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 ../tools/cleanup.sh
+
+echo "asd" > t1.txt
+echo "dsa" > t2.txt
+mkdir f1
+echo "qwe" > f1/t3.txt
+mkdir f1/f2
+echo "ewq" > f1/f2/t4.txt
+echo "rty" > f1/f2/t5.txt
+
 node ../src/cli.js init
